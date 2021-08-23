a57daa978bc50b40480d3422725b82fec0e788fa
Sergi Vaque <galdons@emea.roche.com>
2021-08-23 14:44:15 +0200

    Created a Book class

--- src/main/java/tv/codely/solid_principles/single_responsability_principle/Book.java
+++ src/main/java/tv/codely/solid_principles/single_responsability_principle/Book.java
@@ -1,4 +1,22 @@
 package tv.codely.solid_principles.single_responsability_principle;
 
-public class Book {
+final public class Book {
+
+    private String title;
+    private String author;
+    private String page;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+
 }
